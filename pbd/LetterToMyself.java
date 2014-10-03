6bbc61c98ca8443626deef6f1caad754722b9c45
Steve <steveh7289@gmail.com>
2014-10-03 02:36:44 +0100

    Updated MyInitials.java

---
+++ pbd/LetterToMyself.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Steve on 03/10/2014.
+ */
+
+
+public class LetterToMyself {
+		public static void main(String[] args) { 
+			System.out.println( " +-----------------------------------------------------------------------------+ " );
+			System.out.println( " |                                                                        #### | " );
+			System.out.println( " |                                                                        #### | " );
+			System.out.println( " |                                                                        #### | " );
+			System.out.println( " |                                                                             | " );
+			System.out.println( " |                                                                             | " );
+			System.out.println( " |                                               Bill Gates                    | " );
+			System.out.println( " |                                               1 Microsoft Way               | " );
+			System.out.println( " |                                               Redmond, WA 98104             | " );
+			System.out.println( " |                                                                             | " );
+			System.out.println( " +-----------------------------------------------------------------------------+ " );
+		}
+}
---
+++ pbd/MyInitials.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Steve on 03/10/2014.
+ */
+public class MyInitials {
+        public static void main(String[] args){
+            System.out.println(" SSS          JJJJJ           H   H");
+            System.out.println("S   S           J             H   H");
+            System.out.println("S               J             H   H");
+            System.out.println(" SSS            J             HHHHH");
+            System.out.println("   SS         J J             H   H");
+            System.out.println("S  SS         J J             H   H");
+            System.out.println(" SSS           JJ             H   H");
+    }
+}
