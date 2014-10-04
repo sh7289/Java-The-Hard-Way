54d63af312e2d5045e5e0942c80be1b502407e9e
Steve <steveh7289@gmail.com>
2014-10-04 02:33:05 +0100

    Updated NumbersAndMath.java

--- pbd/NumbersAndMath.java
+++ pbd/NumbersAndMath.java
@@ -5,26 +5,32 @@ public class NumbersAndMath
 {
     public static void main( String [] args )
     {
+        // Counts the number of hens and roosters
         System.out.println("I will now count my chickens!");
 
         System.out.println("Hens: " + (25+30 / 6 ));
         System.out.println("Roosters: " + (100 - 25 * 3 % 4 ));
 
+        // Counts the number of eggs
         System.out.println("Now I will count the eggs!");
 
         System.out.println(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 );
 
+        // Boolean statements
         System.out.println( "Is it true that 3 + 2 < 5 - 7?" );
 
         System.out.println( 3 + 2 < 5 - 7);
 
+        // Addition
         System.out.println( "What is 3 + 2? " + (3 + 2) );
         System.out.println( "What is 5 - 7? " + (5 - 7 ) );
 
+        // Clarifies boolean statements above
         System.out.println( "Oh, that's why it's false." );
 
         System.out.println( "How about some more?" );
 
+        // Checking if a number is greater/less/equal to another number
         System.out.println( "Is it greater than? " + ( 5 > -2 ));
         System.out.println( "Is it greater than or equal to? " + ( 5 >= -2 ));
         System.out.println( "Is it less than or equal to? " + (5 <= -2 ));
